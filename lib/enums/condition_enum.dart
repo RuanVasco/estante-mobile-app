d431ff129f8584b4f45c777306bfce3c9344e7e7
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2025-08-20 15:46:20 -0300

    updated condition enum

--- lib/enums/condition_enum.dart
+++ lib/enums/condition_enum.dart
@@ -1,4 +1,16 @@
 enum ConditionEnum {
-  NEW,
-  USED
+  NEW(value: "new", label: "Novo"),
+  USED(value: "used", label: "Usado");
+
+  final String value;
+  final String label;
+
+  const ConditionEnum({
+    required this.value,
+    required this.label,
+  });
+
+  String getLabel() {
+    return label;
+  }
 }
